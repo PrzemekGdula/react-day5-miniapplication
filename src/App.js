6ee7df0223058885c03467ebd85c40c93da6b7a2
PrzemekGdula <przemo.g12@wp.pl>
2019-04-05 13:53:18 +0200

    UserItem component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AddUser from './AddUser'
+import ListUsers from './ListUsers'
 
 import './logic'
 
 const App = (props) => (
     <div>
+        <ListUsers />
         <AddUser />
     </div>
 )
