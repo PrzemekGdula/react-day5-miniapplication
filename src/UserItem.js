7a9b330fcdf9b3ec577cdddecd7e416075e81e6e
PrzemekGdula <przemo.g12@wp.pl>
2019-04-05 14:11:21 +0200

    Styling UserItem and added user avata icon

--- src/UserItem.js
+++ src/UserItem.js
@@ -1,10 +1,39 @@
 import React from 'react'
 
-const UserItem = (props) => (
-    <div>
-        {props.user.name}
-    </div>
+import userPlaceholder from './user.svg'
 
+const styles = {
+    main: {
+        with: '100%',
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    img: {
+        width: 30,
+        height: 30,
+        borderRadius: '50%',
+    }
+}
+
+const UserItem = (props) => (
+    <div
+        style={styles.main}
+    >
+        <div>
+            <img
+                style={styles.img}
+                src={props.user.avatar || userPlaceholder}
+                alt={props.user.name}
+            />
+        </div>
+        <div>
+            {props.user.name}
+            {props.user.email}
+        </div>
+        <div>
+            <button>X</button>
+        </div>
+    </div >
 )
 
 export default UserItem
