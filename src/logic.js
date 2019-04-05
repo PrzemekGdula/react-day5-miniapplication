328cdf9c2558ea929c6d5d2027ca514a50a08b32
PrzemekGdula <przemo.g12@wp.pl>
2019-04-05 12:20:33 +0200

    exporting logic

--- src/logic.js
+++ src/logic.js
@@ -2,7 +2,7 @@ import uuidv1 from 'uuid/v1'
 
 const LOCAL_STORAGE_KEY = 'jfddl7-users'
 
-class User {
+export class User {
     constructor(name, email, avatar) {
         this.name = name
         this.email = email
@@ -11,9 +11,9 @@ class User {
     }
 }
 
-const loadUsers = () => JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || []
+export const loadUsers = () => JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || []
 
-const saveUser = (user) => {
+export const saveUser = (user) => {
     const currentUsers = loadUsers()
 
     const newUsers = currentUsers.concat(user)
@@ -24,7 +24,7 @@ const saveUser = (user) => {
     )
 }
 
-const deleteUser = (uuid) => {
+export const deleteUser = (uuid) => {
     const currentUsers = loadUsers()
 
     const newUsers = currentUsers.filter((user) => user.uuid !== uuid)
@@ -34,7 +34,3 @@ const deleteUser = (uuid) => {
         JSON.stringify(newUsers)
     )
 }
-
-saveUser(new User('Ala', 'a@a.pl'))
-
-window.deleteUser = deleteUser
